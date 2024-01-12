9fbc5edce5c8675a73e46ccf9bff4b140fcebf8b
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2024-01-12 11:37:14 +0300

    prep for dep

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 const booksRouter = require('./routes/books');
 const PORT = process.env.PORT || 4000;
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 // all routes should be registered after the global middlewares cors and express.json()
 app.use('/api/books', booksRouter);
+
+// THE FOLLOWING BLOCK NEED TO BE AFTER ALL THE BACKEND ROUTES!!!!!!!!!!
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  const buildPath = path.join(__dirname, '../client/dist');
+  app.use(express.static(buildPath));
+
+  app.get('*', (req, res) => res.sendFile(path.join(buildPath, 'index.html')));
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- server/models/book.js
+++ server/models/book.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema(
   {
+    // _id: auto created uuid
     title: { type: String, required: [true, 'Book Title is required!'] },
     author: { type: String, required: [true, 'Book Author is required!'] },
     year: { type: Number, default: 2000 },
